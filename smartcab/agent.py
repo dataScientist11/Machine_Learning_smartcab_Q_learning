1263ab9159fa785848395de7217bf2d4733e7bcb
Jobin <maluplaya08@gmail.com>
2016-08-07 21:40:42 -0500

    corrections 0807

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -13,9 +13,10 @@ class LearningAgent(Agent):
         self.planner = RoutePlanner(self.env, self)  # simple route planner to get next_waypoint
         # TODO: Initialize any additional variables here
         self.Q = defaultdict(list)
-        self.alphaLearn = .8
-        self.gammaDiscount = .8
+        self.alphaLearn = .3
+        self.gammaDiscount = .99
         self.stateAction = None
+        self.epsilon = .2
 
     def reset(self, destination=None):
         self.stateAction = None
@@ -29,17 +30,21 @@ class LearningAgent(Agent):
         deadline = self.env.get_deadline(self)
 
         # TODO: Update state
-        self.state = [inputs['light'],inputs['right'],inputs['left'],deadline, self.next_waypoint]
-        
+        self.state = [inputs['light'],inputs['right'],inputs['left'], self.next_waypoint]
+        # print 'this is waypoint {}'.format(self.next_waypoint)
         # TODO: Select action according to your policy
+
         action = None
         maxQ = 0
-        for k,v in self.Q.items():
-            if k[0:4] == self.state and v > maxQ:
-                action = k[5]
-                maxQ = v
-        if action == None:
+        if random.random() < self.epsilon:
             action = random.choice([None, 'forward', 'left', 'right']) 
+        else:
+            for k,v in self.Q.items():
+                if k[0:3] == self.state and v > maxQ:
+                    action = k[4]
+                    maxQ = v
+            if action == None:
+                action = random.choice([None, 'forward', 'left', 'right']) 
 
 
         # Execute action and get reward
@@ -58,7 +63,7 @@ class LearningAgent(Agent):
 
         #print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
         
-        self.stateAction = [inputs['light'],inputs['right'],inputs['left'],deadline, self.next_waypoint, action]
+        self.stateAction = [inputs['light'],inputs['right'],inputs['left'], self.next_waypoint, action]
 
 def run():
     """Run the agent for a finite number of trials."""
@@ -66,11 +71,11 @@ def run():
     # Set up environment and agent
     e = Environment()  # create environment (also adds some dummy traffic)
     a = e.create_agent(LearningAgent)  # create agent
-    e.set_primary_agent(a, enforce_deadline=True)  # specify agent to track
+    e.set_primary_agent(a, enforce_deadline=False)  # specify agent to track
     # NOTE: You can set enforce_deadline=False while debugging to allow longer trials
 
     # Now simulate it
-    sim = Simulator(e, update_delay=0.01, display=True)  # create simulator (uses pygame when display=True, if available)
+    sim = Simulator(e, update_delay=0.001, display=False)  # create simulator (uses pygame when display=True, if available)
     # NOTE: To speed up simulation, reduce update_delay and/or set display=False
 
     sim.run(n_trials=100)  # run for a specified number of trials
